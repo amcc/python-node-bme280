a7ee72d0e823522cf8add779217adffb1c6e6908
amcc <alistairmcclymont@gmail.com>
2019-09-11 21:25:08 +0100

    fix python output

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ var process = spawn("python", ["bme280.py"]);
 
 process.stdout.on("data", function(chunk) {
   var textChunk = chunk.toString("utf8"); // buffer to string
+  // preserve newlines, etc - use valid JSON
+  s = textChunk
+    .replace(/\\n/g, "\\n")
+    .replace(/\\'/g, "\\'")
+    .replace(/\\"/g, '\\"')
+    .replace(/\\&/g, "\\&")
+    .replace(/\\r/g, "\\r")
+    .replace(/\\t/g, "\\t")
+    .replace(/\\b/g, "\\b")
+    .replace(/\\f/g, "\\f");
+  // remove non-printable and other non-valid JSON chars
+  s = s.replace(/[\u0000-\u0019]+/g, "");
   // console.log(textChunk);
   // js read this
-  var json = JSON.parse(textChunk);
+  var json = JSON.parse(s);
 
-  console.log(json)
+  console.log(json);
   // util.log(textChunk);
 });
