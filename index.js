23a38c0724e7b2bdd96fd5680fe172e6d4dc8338
amcc <alistairmcclymont@gmail.com>
2019-09-11 21:11:40 +0100

    json parsing

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // spawn_python.js
-var util = require("util");
+// var util = require("util");
 
 var spawn = require("child_process").spawn;
 var process = spawn("python", ["bme280.py"]);
 
-util.log("readingin");
+// util.log("readingin");
 
 process.stdout.on("data", function(chunk) {
   var textChunk = chunk.toString("utf8"); // buffer to string
   // console.log(textChunk);
-  util.log(textChunk);
+  // js read this
+  var json = JSON.parse(textChunk);
+
+  console.log(json)
+  // util.log(textChunk);
 });
