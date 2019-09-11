de2dd8ca30f6dcb68c7f07484a4ed61a9496ed23
amcc <alistairmcclymont@gmail.com>
2019-09-11 22:34:17 +0100

    added firebase and timer for the data

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
+const FirebaseUpdate = require('./firebase.js');
+
+// check the bme280 with a delay
+timeDelay = 1000;
+let timer = setInterval(getEnv, timeDelay);
+
 // spawn the python to do the hard work
-
 var spawn = require("child_process").spawn;
-var process = spawn("python", ["bme280.py"]);
 
-process.stdout.on("data", function(chunk) {
-  var textChunk = chunk.toString("utf8"); // buffer to string
-  // preserve newlines, etc - use valid JSON
-  s = textChunk
-    .replace(/\\n/g, "\\n")
-    .replace(/\\'/g, "\\'")
-    .replace(/\\"/g, '\\"')
-    .replace(/\\&/g, "\\&")
-    .replace(/\\r/g, "\\r")
-    .replace(/\\t/g, "\\t")
-    .replace(/\\b/g, "\\b")
-    .replace(/\\f/g, "\\f");
-  // remove non-printable and other non-valid JSON chars
-  s = s.replace(/[\u0000-\u0019]+/g, "");
-  // make JSON
-  var json = JSON.parse(s);
+// get the latest environment variables
+function getEnv(){
+  // to debug use python.py below
+  var process = spawn("python", ["bme280.py"]);
+  process.stdout.on("data", function(chunk) {
+    var textChunk = chunk.toString("utf8"); // buffer to string
+    // preserve newlines, etc - use valid JSON
+    s = textChunk
+      .replace(/\\n/g, "\\n")
+      .replace(/\\'/g, "\\'")
+      .replace(/\\"/g, '\\"')
+      .replace(/\\&/g, "\\&")
+      .replace(/\\r/g, "\\r")
+      .replace(/\\t/g, "\\t")
+      .replace(/\\b/g, "\\b")
+      .replace(/\\f/g, "\\f");
+    // remove non-printable and other non-valid JSON chars
+    s = s.replace(/[\u0000-\u0019]+/g, "");
+    // make JSON
+    var json = JSON.parse(s);
+  
+    console.log(json);
+    FirebaseUpdate.updateDatabase(json);
+  });
+}
 
-  console.log(json);
-});
+// process.on('SIGINT', () => {
+//   // On CTRL+C force a clean exit to use above
+//   process.exit(0);
+// });
+
+// kill() {
+//   clearTimeout(timer);
+// }
---
+++ python.py
@@ -0,0 +1,7 @@
+# a simple python example that you want to use
+# in nodeJS and get the vales as an array
+
+temperature = 28
+pressure = 1010
+humidity = 55
+print '{"temperature":',temperature,',"pressure":',pressure,',"humidity":',humidity,'}'
